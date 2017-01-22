2a91e2fc14fe8f3cb794719a5123a271b9a936d7
Jason Liu <jason.sean.liu@gmail.com>
2017-01-21 16:36:49 -0800

    Added unit testing from last assignment and updated Makefile; we can begin development now

--- Makefile
+++ Makefile
@@ -2,21 +2,33 @@
 
 CC = g++
 CFLAGS = -std=c++0x -g -Wall
+GTEST_DIR = googletest/googletest
+TEST_CFLAGS = -std=c++0x -isystem $(GTEST_DIR)/include -pthread
 
 TARGET = config_parser
+TEST_TARGET = config_parser_test
 
-PARSER_OBJECTS = config_parser.o config_parser_main.o
+PARSER_SOURCE = config_parser.cc config_parser_main.cc
+TEST_SOURCE = config_parser_test.cc config_parser.cc $(GTEST_DIR)/src/gtest_main.cc
 
 all: parser
 
-parser:	$(PARSER_OBJECTS)
-	$(CC) $(CFLAGS) -o $(TARGET) $(PARSER_OBJECTS)
+parser:	$(PARSER_SOURCE)
+	$(CC) $(CFLAGS) -o $(TARGET) $(PARSER_SOURCE)
 
-config_parser.o: config_parser.cc config_parser.h
-	$(CC) $(CFLAGS) -c config_parser.cc
+check: tests
+	./$(TEST_TARGET)
 
-config_parser_main.o: config_parser_main.cc
-	$(CC) $(CFLAGS) -c config_parser_main.cc
+tests: $(TEST_OBJECTS) libgtest.a
+	$(CC) $(TEST_CFLAGS) $(TEST_SOURCE) libgtest.a -o $(TEST_TARGET)
+
+libgtest.a: gtest-all.o
+	ar -rv $@ $^
+
+gtest-all.o: $(GTEST_DIR)/src/gtest-all.cc $(GTEST_DIR)/include/gtest/gtest.h
+	$(CC) $(TEST_CFLAGS) -I$(GTEST_DIR) -c $(GTEST_DIR)/src/gtest-all.cc
 
 clean:
-	$(RM) *.o *~ $(TARGET)
+	$(RM) *.o *~ *.a $(TARGET) $(TEST_TARGET)
+
+.PHONY: all parser check clean
---
+++ config_parser_test.cc
@@ -0,0 +1,80 @@
+#include "gtest/gtest.h"
+#include "config_parser.h"
+
+TEST(NginxConfigParserTest, SimpleConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("config", &out_config);
+
+  EXPECT_TRUE(success);
+}
+
+// foo bar;
+TEST(NginxConfigTest, ToString) {
+	NginxConfigStatement statement;
+	statement.tokens_.push_back("foo");
+	statement.tokens_.push_back("bar");
+	EXPECT_EQ(statement.ToString(0), "foo bar;\n");
+}
+
+class NginxStringConfigTest : public ::testing::Test {
+protected:
+	bool ParseString(const std::string config_string) {
+		std::stringstream config_stream(config_string);
+		return parser_.Parse(&config_stream, &out_config_);
+	}
+	NginxConfigParser parser_;
+	NginxConfig out_config_;
+};
+
+TEST_F(NginxStringConfigTest, AnotherSimpleConfig) {
+	EXPECT_TRUE(ParseString("foo bar;"));
+	EXPECT_EQ(1, out_config_.statements_.size())
+		<< "Config has one statements";
+	EXPECT_EQ("foo", out_config_.statements_.at(0)->tokens_.at(0));
+}
+
+TEST_F(NginxStringConfigTest, SimplerConfig) {
+	EXPECT_TRUE(ParseString("foo;"));
+}
+
+TEST_F(NginxStringConfigTest, EmptyNestedConfig) {
+	EXPECT_TRUE(ParseString("boo far; foo {}"));
+}
+
+TEST_F(NginxStringConfigTest, NoConfig) {
+	EXPECT_TRUE(ParseString(""));
+}
+
+TEST_F(NginxStringConfigTest, InvalidConfig) {
+	EXPECT_FALSE(ParseString("foo bar"));
+}
+
+TEST_F(NginxStringConfigTest, NestedConfig) {
+	EXPECT_TRUE(ParseString("server { listen 80; }"));
+}
+
+TEST_F(NginxStringConfigTest, InvalidNestedConfig) {
+	EXPECT_FALSE(ParseString("server { listen 80; "));
+}
+
+TEST_F(NginxStringConfigTest, MultipleNestedConfig) {
+	EXPECT_TRUE(ParseString("server { foo { listen 80; } }"));
+}
+
+TEST_F(NginxStringConfigTest, MultiNestMultiStatementConfig) {
+	EXPECT_TRUE(ParseString("server { foo { listen 80; } abc def; }"));
+}
+
+TEST_F(NginxStringConfigTest, CommentConfig) {
+	EXPECT_TRUE(ParseString("# nothing to see here"));
+}
+
+TEST_F(NginxStringConfigTest, QuoteConfig) {
+	EXPECT_TRUE(ParseString("foo \"bar\";"));
+}
+
+TEST_F(NginxStringConfigTest, InvalidQuoteConfig) {
+	EXPECT_FALSE(ParseString("foo \"bar;"));
+}
