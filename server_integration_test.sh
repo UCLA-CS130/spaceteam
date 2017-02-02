915d5e1f8768c8edd0a85132b230ee1132161ff0
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-02-02 00:15:45 -0800

    simple integration test with curl added

--- server_integration_test.sh
+++ server_integration_test.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+# assistance from http://stackoverflow.com/questions/25320928/how-to-capture-the-output-of-curl-to-variable-in-bash
+# in order to capture the curl command
 
-expected="who knows"
+url="http://localhost:1890"
 
 # Start the webserver
-./webserver --port 1880 &
+./webserver test_config &
 
-# Run the test
-if [ curl http://localhost:1880 | grep $expected x = 'x'];
+output=$(curl -I $url | head -n 1| cut -d $' ' -f2)
+
+# Checks the "OK" status code from HTTP
+if [ $output -eq "200" ];
 	then
-		echo "FAILED"
-	else
 		echo "SUCCESS"
+	else
+		echo "FAILED"
 fi
 
 # Stop the webserver
