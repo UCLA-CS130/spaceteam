ff6ad3921b38daaa418e488dd5fb38facd247e84
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-02-06 00:30:45 -0800

    fixed accidental edit to test

--- server_test.cc
+++ server_test.cc
@@ -9,4 +9,7 @@
 
 // Server initialization test
 TEST(ServerTest, SimpleServerTest) {
+	boost::asio::io_service io_service;
+	// "config" should eventually be moved to a global constant
+	Server server(io_service, "config");
 }
