2eb65d2a33f4802ccf4f7427d6a9ecfc2ef29882
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-02-08 00:03:49 -0800

    pushed fixes

--- server_integration_test.sh
+++ server_integration_test.sh
@@ -21,32 +21,20 @@ if [ $output -eq "200" ];
 		echo "FAILED: TO RECEIVE A RESPONSE"
 fi
 
-# Stop the webserver
-# kill %1
-
 # === Integration Test 2 ===
 # Checking that the server can return a static file
-# Start the webserver
-# ./webserver test_config & 
-
 output2=$(curl http://localhost:1890/static/test.html | diff example_files/test.html - )
 
 # Checks that the diff produces no output, since the files should be the same.
-if [$output2 -eq ""];
+if [ $output2 -eq ""];
 	then
 		echo "SUCCESS: SERVED TEST.HTML FILE"
 	else
 		echo "FAILED: TO SERVE TEST.HTML FILE"
 fi
 
-#Stop the webserver
-# kill %1
-
 # === Integration Test 3 ===
 # Checking that the server can't return a static file, returning 404
-# Start the webserver
-# ./webserver test_config & 
-
 output3=$(curl -I http://localhost:1890/static/notreal.html | head -n 1 | cut -d $' ' -f2)
 
 # Checks that the diff produces no output, since the files should be the same.
