85191e1cc5e40bedf8f53cb66609f7c58b698df5
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-02-07 17:49:42 -0800

    adds request parser tests

--- connection_test.cc
+++ connection_test.cc
@@ -9,10 +9,19 @@
 class ConnectionTest : public ::testing::Test {
  protected:
   ConnectionTest() {
-    connection_ = Connection::create(io_service_);
+    injectTestMaps();
+    connection_ = Connection::create(io_service_, &echo_map, &static_map);
+  }
+  
+  // Inject the maps created before connection is initialized.
+  void injectTestMaps() {
+    echo_map["/echo"] =  "";
+    static_map["/static"] = "static_files";
   }
 
   boost::asio::io_service io_service_;
+  std::map<std::string, std::string> echo_map;
+  std::map<std::string, std::string> static_map;
   Connection::pointer connection_;
 };
 
---
+++ request_parser_test.cc
@@ -0,0 +1,56 @@
+#define TEST_REQUEST_PARSER
+  
+#include "gtest/gtest.h"
+#include "request_parser.h"
+#include "request.h"
+
+#define BUFFER_SIZE 1024 
+
+class RequestParserTest : public ::testing::Test {
+  protected:
+  RequestParserTest() {
+  }
+
+  void runParse() {
+    std::tie(result, std::ignore) = parser_.parse(
+        request, test_buffer.begin(), test_buffer.end()); 
+  }
+
+  void setUpRequest(std::string buffer) {
+    test_buffer = buffer;
+    runParse();
+  }
+
+  void properRequest() {
+    setUpRequest("GET /echo/hello/there.html HTTP/1.1\r\n" + '\0');
+  }
+
+  std::string test_buffer;
+  RequestParser parser_;
+  Request request;
+  RequestParser::result_type result;
+};
+
+TEST_F(RequestParserTest, EmptyRequest) { 
+  std::tie(result, std::ignore) = parser_.parse(
+      request, test_buffer.begin(), test_buffer.end());
+  EXPECT_EQ(result, RequestParser::result_type::indeterminate);
+}
+
+TEST_F(RequestParserTest, CorrectRequest) { 
+  properRequest();
+  EXPECT_EQ(result, RequestParser::result_type::indeterminate);
+}
+
+// Correct Handler + File Paths
+TEST_F(RequestParserTest, CorrectPathsHandlerRequest) {
+  properRequest();
+  EXPECT_EQ(request.handler_path, "/echo");
+  EXPECT_EQ(request.file_path, "/hello/there.html");
+  EXPECT_EQ(result, RequestParser::result_type::indeterminate);
+}
+
+TEST_F(RequestParserTest, IncorrectRequest) { 
+  setUpRequest("asdfj<kl;");
+  EXPECT_EQ(result, RequestParser::result_type::bad);
+}
