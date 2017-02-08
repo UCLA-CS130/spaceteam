d5c0ce5c4217f310199d00ebd3b4211a1c91de71
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-02-07 23:40:06 -0800

    Added simple integration test for serving files

--- server_integration_test.sh
+++ server_integration_test.sh
@@ -2,20 +2,42 @@
 # assistance from http://stackoverflow.com/questions/25320928/how-to-capture-the-output-of-curl-to-variable-in-bash
 # in order to capture the curl command
 
-url="http://localhost:1890"
+local_url="http://localhost:1890"
+test_file_url = "http://localhost:1890/static/test.html"
+
+# === Integration Test 1 ===
+# Checking that the server starts and returns a response.
 
 # Start the webserver
 ./webserver test_config &
 
-output=$(curl -I $url | head -n 1| cut -d $' ' -f2)
+output=$(curl -I $local_url | head -n 1| cut -d $' ' -f2)
 
 # Checks the "OK" status code from HTTP
 if [ $output -eq "200" ];
 	then
-		echo "SUCCESS"
+		echo "SUCCESS: RECEIVED A RESPONSE"
 	else
-		echo "FAILED"
+		echo "FAILED: TO RECEIVE A RESPONSE"
 fi
 
 # Stop the webserver
+kill %1
+
+# === Integration Test 2 ===
+# Checking that the server can return a static file
+# Start the webserver
+./webserver test_config & 
+
+output2=$(curl http://localhost:1890/static/test.html | diff example_files/test.html -)
+
+# Checks that the diff produces no output, since the files should be the same.
+if [ $output -eq ""];
+	then
+		echo "SUCCESS: SERVED TEST.HTML FILE"
+	else
+		echo "FAILED: TO SERVE TEST.HTML FILE"
+fi
+
+#Stop the webserver
 kill %1
