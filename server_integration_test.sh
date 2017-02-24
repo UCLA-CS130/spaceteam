105714c94f3760b13de987c3dba2c50e2a8995e0
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-02-23 19:03:40 -0800

    fixed integration tests

--- server_integration_test.sh
+++ server_integration_test.sh
@@ -2,16 +2,16 @@
 # assistance from http://stackoverflow.com/questions/25320928/how-to-capture-the-output-of-curl-to-variable-in-bash
 # in order to capture the curl command
 
-local_url="http://localhost:1890"
+echo_url="http://localhost:2020/echo"
 
 # === Integration Test 1 ===
-# Checking that the server starts and returns a response.
+# Checking that the server starts and echos a response.
 
 # Start the webserver
 ./webserver test_config &
 
 # Tests for status code
-output=$(curl -I $local_url | head -n 1| cut -d $' ' -f2)
+output=$(curl -I $echo_url | head -n 1| cut -d $' ' -f2)
 
 # Checks the "OK" status code from HTTP
 if [ $output -eq "200" ];
@@ -21,11 +21,11 @@ if [ $output -eq "200" ];
 		echo "FAILED: TO RECEIVE A RESPONSE"
 fi
 
-# === Integration Test 2 ===
-# Checking that the server can return a static file
-output2=$(curl http://localhost:1890/static/test.html | diff example_files/test.html - )
+# # === Integration Test 2 ===
+# # Checking that the server can return a static file
+output2=$(curl http://localhost:2020/test.html | diff example_files/test.html - )
 
-# Checks that the diff produces no output, since the files should be the same.
+# # Checks that the diff produces no output, since the files should be the same.
 if [ -z $output2 ];
 	then
 		echo "SUCCESS: SERVED TEST.HTML FILE"
@@ -35,7 +35,7 @@ fi
 
 # === Integration Test 3 ===
 # Checking that the server can't return a static file, returning 404
-output3=$(curl -I http://localhost:1890/static/notreal.html | head -n 1 | cut -d $' ' -f2)
+output3=$(curl -I http://localhost:2020/notreal.html | head -n 1 | cut -d $' ' -f2)
 
 # Checks that the diff produces no output, since the files should be the same.
 if [ $output3 -eq "404" ];
