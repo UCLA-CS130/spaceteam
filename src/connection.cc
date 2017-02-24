9261ac7009049dff4bbe0f38e07640536630aa57
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-02-23 19:21:45 -0800

    edited error messages

--- src/connection.cc
+++ src/connection.cc
@@ -71,11 +71,12 @@ bool Connection::handle_read(const boost::system::error_code& error,
   if (request_handler != NULL) {
     std::cerr << "Did not find any Request Handlers matching this path." << std::endl;
     request_handler = new NotFoundHandler();
+  } else {
+    std::cerr << "Found Request Handler matching this path." << std::endl;
   }
 
   Response response;
   request_handler->HandleRequest(*request, &response);
-  std::cerr << "Found Request Handler matching this path." << std::endl;
   do_write(response);
   return true; // success
 }
