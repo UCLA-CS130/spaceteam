ded5b4e7ca3f7b81b686e0d2616ef357cca40d8d
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-03-01 14:45:02 -0800

    added static handler

--- src/static_handler_test.cc
+++ src/static_handler_test.cc
@@ -10,7 +10,7 @@
 class StaticHandlerTest : public ::testing::Test {
  protected:
   StaticHandlerTest() {
-    ParseString("test_config");
+    ParseString("root ./example_files;");
     status = static_handler.Init(uri_prefix, config);
     request = Request::Parse(TEST_BUFFER);
   }
@@ -25,7 +25,7 @@ class StaticHandlerTest : public ::testing::Test {
   StaticHandler static_handler;
   const std::string uri_prefix = "/static";
   const std::string TEST_BUFFER = "GET /static/test.html HTTP/1.1\r\nUser-Agent: curl/7.35.0\r\nHost: localhost:2020\r\nAccept: */*\r\n\r\n";
-  const std::string EXPECTED_RESPONSE = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nGET /echo HTTP/1.1\r\nAccept-Language: en-us\r\n\r\nname=1";
+  std::string EXPECTED_RESPONSE = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title>Server Test</title>\r\n</head>\r\n<body>\r\n<h1>Testing testing 1 2 3...</h1>\r\n<p>Looks like the server is working!</p>\r\n</body>\r\n</html>";
   RequestHandler::Status status;
   std::unique_ptr<Request> request;
   Response response;
@@ -41,9 +41,6 @@ TEST_F(StaticHandlerTest, NullResponse) {
 
 TEST_F(StaticHandlerTest, HandleProperRequestTest) {
   EXPECT_EQ(static_handler.HandleRequest(*request, &response), RequestHandler::Status::OK);
-  // EXPECT_EQ(response.GetResponseCode(), Response::OK);
-  std::cout << "URI IS: " + request->uri() << std::endl;
-  std::cout << request->raw_request() << std::endl;
-  std::cout << response.ToString();
-  // EXPECT_EQ(response.ToString(), EXPECTED_RESPONSE);
+  EXPECT_EQ(response.GetResponseCode(), Response::OK);
+  EXPECT_EQ(response.ToString(), EXPECTED_RESPONSE);
 }
