7a549c35b8ec669c1d33e8cfc69a3b3d9429c895
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-03-01 01:26:04 -0800

    modified int tests to make modular

--- multithread_test.py
+++ multithread_test.py
@@ -7,40 +7,47 @@ import time
 
 PORT = 2020
 BUFFER_LEN = 1024
+HOLD_REQUEST = "GET /hold HTTP/1.1"
 STATIC_REQUEST = "GET /static/test.html HTTP/1.1\r\n\r\n"
 EXPECTED_STATIC_RESPONSE = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title>Server Test</title>\r\n</head>\r\n<body>\r\n<h1>Testing testing 1 2 3...</h1>\r\n<p>Looks like the server is working!</p>\r\n</body>\r\n</html>"
 
-def set_socket():
+# Sets up socket for connection
+def setup_socket():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	sock.connect(('localhost', PORT))
 	return sock	
 
-def hold_connect():
-	sock = set_socket()
-	sock.send("GET /hold HTTP/1.1")
-	sock.send("\r\n\r\n")
+def receive(sock):
 	data = sock.recv(BUFFER_LEN)
 	sock.close()
+	return data
+
+def hold_connect():
+	sock = setup_socket()
+	sock.send(HOLD_REQUEST)
+	sock.send("\r\n\r\n")
+	receive(sock)
 	return
 
-def connect(REQ, EX_RESP, ec):
-	sock = set_socket()
-	sock.send(REQ)
-	receive = sock.recv(BUFFER_LEN)
-	sock.close()
+def connect(request, expected_response, error_code):
+	sock = setup_socket()
+	sock.send(request)
+	response = receive(sock)
 
-	if (receive != EX_RESP):
-		print "Error: ", repr(receive)
-		ec = 1
+	if (response != expected_response):
+		print "Error: ", repr(response)
+		error_code = 1
 		return
 	print "Connection Successful!"
-	ec = 0
+	error_code = 0
 
 def two_connections_test():
 	print "Starting hold request thread"
 	hold = Thread(target = hold_connect, args=())
 	hold.start()
+
+	time.sleep(3)
 
 	print "Starting static request thread"
 	static_error = 0
