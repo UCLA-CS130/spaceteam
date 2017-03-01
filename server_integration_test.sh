e695d06d583cc659c43a02d76255c9a6fa5fe5ba
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-03-01 00:11:35 -0800

    Edited integration test so it exits with the results of testing

--- server_integration_test.sh
+++ server_integration_test.sh
@@ -10,12 +10,14 @@
 
 # Tests for status code
 output=$(curl -I http://localhost:2020/echo | head -n 1| cut -d $' ' -f2)
+$result = 1
 
 # Checks the "OK" status code from HTTP
 if [ $output -eq "200" ];
 	then
 		echo "SUCCESS: RECEIVED A RESPONSE"
 	else
+		$result = 0
 		echo "FAILED: TO RECEIVE A RESPONSE"
 fi
 
@@ -28,6 +30,7 @@ if [ -z $static_output ];
 	then
 		echo "SUCCESS: SERVED TEST.HTML FILE"
 	else
+		$result = 0
 		echo "FAILED: TO SERVE TEST.HTML FILE"
 fi
 
@@ -40,16 +43,17 @@ if [ $not_found_output -eq "404" ];
 	then
 		echo "SUCCESS: RETURNED 404 NOT FOUND ERROR"
 	else
+		$result = 0
 		echo "FAILED: DID NOT RETURN 404 ERROR"
 fi
 
 # === Integration Test 4 ===
 # Checking that the server can handle multiple connections at once using multithreading
 
-# thread_output=$(curl -I http://localhost:2020/hold | curl -I http://localhost:2020/hold | (sleep 20 && kill %1))
+# thread_output=$(curl -I http://localhost:2020/hold & (curl -I http://localhost:2020/echo) | (sleep 10 & pkill webserver))
 
 # # Checks that the diff produces no output, since the files should be the same.
-# if [[ $thread_output == *"Waiting on HoldingHandler"* && $thread_output == *"200"*]];
+# if [ $thread_output -eq "Waiting on HoldingHandler" ];
 # 	then
 # 		echo "SUCCESS: HANDLED A SECOND CONNECTION SIMULTANEOUSLY"
 # 	else
@@ -57,4 +61,7 @@ fi
 # fi
 
 #Stop the webserver
-kill %1
+pkill webserver
+
+# Will return 1 if all tests passed, will return 0 if one fails
+exit $result
