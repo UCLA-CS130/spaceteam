7619a62b66e1c3f4de45318a5a51081a205ffbab
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-03-01 16:37:53 -0800

    Update not_found_handler_test.cc

--- src/not_found_handler_test.cc
+++ src/not_found_handler_test.cc
@@ -10,12 +10,12 @@
 class NotFoundHandlerTest : public ::testing::Test {
  protected:
   NotFoundHandlerTest() {
-    status = not_found_handler_.Init(uri_prefix_, config_);
+    const std::string uri_prefix;
+    const NginxConfig config;
+    status_ = not_found_handler_.Init(uri_prefix, config);
   }
 
   NotFoundHandler not_found_handler_;
-  const std::string uri_prefix_;
-  const NginxConfig config_;
   const std::string EXPECTED_RESPONSE = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<!doctype html><html><body><h1>404 Not Found</h1></body></html>";
   RequestHandler::Status status_;
 };
@@ -27,7 +27,7 @@ TEST_F(NotFoundHandlerTest, InitTest) {
 TEST_F(NotFoundHandlerTest, HandleProperRequestTest) {
   Request request;
   Response response;
-  EXPECT_EQ(not_found_handler.HandleRequest(request, &response), RequestHandler::Status::OK);
+  EXPECT_EQ(not_found_handler_.HandleRequest(request, &response), RequestHandler::Status::OK);
   EXPECT_EQ(response.GetResponseCode(), Response::NOT_FOUND);
   EXPECT_EQ(response.ToString(), EXPECTED_RESPONSE);
 }
