024de29e6cb0872c219d548442fed4c3941a7058
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-03-01 16:03:35 -0800

    Update echo_handler_test.cc

--- src/echo_handler_test.cc
+++ src/echo_handler_test.cc
@@ -10,26 +10,26 @@
 class EchoHandlerTest : public ::testing::Test {
  protected:
   EchoHandlerTest() {
-    status = echo_handler.Init(uri_prefix, config);
+    status = echo_handler.Init(uri_prefix_, config_);
     request = Request::Parse(TEST_BUFFER);
   }
 
-  EchoHandler echo_handler;
-  const std::string uri_prefix;
-  const NginxConfig config;
+  EchoHandler echo_handler_;
+  const std::string uri_prefix_;
+  const NginxConfig config_;
   const std::string TEST_BUFFER = "GET /echo HTTP/1.1\r\nAccept-Language: en-us\r\n\r\nname=1";
   const std::string EXPECTED_RESPONSE = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nGET /echo HTTP/1.1\r\nAccept-Language: en-us\r\n\r\nname=1";
-  RequestHandler::Status status;
-  std::unique_ptr<Request> request;
+  RequestHandler::Status status_;
+  std::unique_ptr<Request> request_;
 };
 
 TEST_F(EchoHandlerTest, InitTest) {
-  EXPECT_EQ(status, RequestHandler::Status::OK);
+  EXPECT_EQ(status_, RequestHandler::Status::OK);
 }
 
 TEST_F(EchoHandlerTest, HandleProperRequestTest) {
   Response response;
-  EXPECT_EQ(echo_handler.HandleRequest(*request, &response), RequestHandler::Status::OK);
+  EXPECT_EQ(echo_handler_.HandleRequest(*request_, &response), RequestHandler::Status::OK);
   EXPECT_EQ(response.GetResponseCode(), Response::OK);
   EXPECT_EQ(response.ToString(), EXPECTED_RESPONSE);
 }
