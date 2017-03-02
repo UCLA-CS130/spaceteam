d01f2361695d2cc01629f45a99b8a935462e9494
Crystal Hsieh <crystalhsieh7@gmail.com>
2017-03-01 16:41:10 -0800

    Update static_handler_test.cc

--- src/static_handler_test.cc
+++ src/static_handler_test.cc
@@ -11,36 +11,36 @@ class StaticHandlerTest : public ::testing::Test {
  protected:
   StaticHandlerTest() {
     ParseString("root ./example_files;");
-    status = static_handler.Init(uri_prefix, config);
-    request = Request::Parse(TEST_BUFFER);
+    status_ = static_handler.Init(uri_prefix_, config_);
+    request_ = Request::Parse(TEST_BUFFER);
   }
 
   bool ParseString(const std::string config_string) {
     std::stringstream config_stream(config_string);
-    return parser.Parse(&config_stream, &config);
+    return parser_.Parse(&config_stream, &config_);
   }
-  NginxConfigParser parser;
-  NginxConfig config;
+  NginxConfigParser parser_;
+  NginxConfig config_;
 
-  StaticHandler static_handler;
-  const std::string uri_prefix = "/static";
+  StaticHandler static_handler_;
+  const std::string uri_prefix_ = "/static";
   const std::string TEST_BUFFER = "GET /static/test.html HTTP/1.1\r\nUser-Agent: curl/7.35.0\r\nHost: localhost:2020\r\nAccept: */*\r\n\r\n";
   std::string EXPECTED_RESPONSE = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title>Server Test</title>\r\n</head>\r\n<body>\r\n<h1>Testing testing 1 2 3...</h1>\r\n<p>Looks like the server is working!</p>\r\n</body>\r\n</html>";
-  RequestHandler::Status status;
-  std::unique_ptr<Request> request;
-  Response response;
+  RequestHandler::Status status_;
+  std::unique_ptr<Request> request_;
+  Response response_;
 };
 
 TEST_F(StaticHandlerTest, InitTest) {
-  EXPECT_EQ(status, RequestHandler::Status::OK);
+  EXPECT_EQ(status_, RequestHandler::Status::OK);
 }
 
 TEST_F(StaticHandlerTest, NullResponse) {
-  EXPECT_EQ(static_handler.HandleRequest(*request, nullptr), RequestHandler::Status::ERROR);
+  EXPECT_EQ(static_handler_.HandleRequest(*request_, nullptr), RequestHandler::Status::ERROR);
 }
 
 TEST_F(StaticHandlerTest, HandleProperRequestTest) {
-  EXPECT_EQ(static_handler.HandleRequest(*request, &response), RequestHandler::Status::OK);
-  EXPECT_EQ(response.GetResponseCode(), Response::OK);
-  EXPECT_EQ(response.ToString(), EXPECTED_RESPONSE);
+  EXPECT_EQ(static_handler_.HandleRequest(*request_, &response_), RequestHandler::Status::OK);
+  EXPECT_EQ(response_.GetResponseCode(), Response::OK);
+  EXPECT_EQ(response_.ToString(), EXPECTED_RESPONSE);
 }
